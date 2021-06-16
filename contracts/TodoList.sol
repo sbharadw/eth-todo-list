a8bde94ca78d58a7b80c64bceb6ee2a623ea639f
Satyam B <sbharadw@kent.edu>
2021-06-15 23:50:41 -0400

    adding more changes

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -5,4 +5,21 @@ contract TodoList {
 
 uint public taskCount = 0;
 
+constructor() public {
+        createTask("Satyam's first toDo List on Blockchain!");
+      }
+
+struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+mapping(uint => Task) public tasks;
+
+function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
 }
